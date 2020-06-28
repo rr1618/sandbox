0d49871c4c389392e7e3ab467b4ee865bea15321
ynot-krats <53901753+ynot-krats@users.noreply.github.com>
2020-06-29 02:03:51 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./styles.css";
-
+import { Button } from "antd";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <Button type="primary">Primary Button</Button>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
